b924c0c4964901baced8799c30afc7749071b6e8
AbdullahAtilla <abdullah.atilla@outlook.com>
2017-08-18 17:30:52 +0300

    Take connection arguments from config file and if an argument from config file is not already existing in database, insert it.

--- DBManager.py
+++ DBManager.py
@@ -1,15 +1,15 @@
 import mysql.connector
 from mysql.connector import errorcode
-
+import ConfigManager
 
 ########################################
 ##CONNECTION ARGUMENTS #################
 ########################################
 
 config = {
-	'user'             : 'root',
-	'password'         : '00000.Aa',
-	'host'             : '127.0.0.1',
+	'user'             : ConfigManager.ConfigSectionMap("Basic_Conf")['databaseuser'],
+	'password'         : ConfigManager.ConfigSectionMap("Basic_Conf")['databasepass'],
+	'host'             : ConfigManager.ConfigSectionMap("Basic_Conf")['host'],
 	'database'         : 'EOT_DATA',
 	'raise_on_warnings': True,
 }
@@ -19,14 +19,14 @@ config = {
 
 
 
-##################
-##   VARIABLES  ## 
-##################
-statcode   =  100;
-cameraid   =  1;
-unitid     =  1;
-
-###################
+##############################
+# VARIABLES FROM CONFIG FILE## 
+##############################
+statcode        = 100; ##Default code; 'Unknown'
+cameraNode      = int(ConfigManager.ConfigSectionMap("Basic_Conf")['cameranode']);
+unitType        = str(ConfigManager.ConfigSectionMap("Basic_Conf")['unit']);
+locationAddress = str(ConfigManager.ConfigSectionMap("Basic_Conf")['location']);
+############################
 
 
 
@@ -53,7 +53,6 @@ def connect():
 	    print(err)
 
 	return con  ##return connection
-
 ###END OF THE FUNCTION
 ####################################################################
 
@@ -81,31 +80,83 @@ def insert_data(mtime, mdata, photoPath):
 	############
 	##QUERIES###
 	############
-	add_photo = ("INSERT INTO EOT_DATA.Photo(path) VALUES ('%s')" % photoPath ) ##Insert photo query
+	add_photo = ("INSERT INTO EOT_DATA.Photo (path, cameraID) VALUES (%(path)s , %(cameraid)s)") ##Insert photo query
 
-	selectPhotoID = ("SELECT ID FROM EOT_DATA.Photo WHERE path LIKE '%s'" % photoPath) ##Select photoID via its path query
+	selectPhotoID = ("SELECT ID FROM EOT_DATA.Photo WHERE Photo.path LIKE '%s'" % photoPath) ##Select photoID via its path query
 
-	selectStatID  = ("SELECT ID FROM MStatus WHERE statCode = %s" % statcode) ##Select status ID via statCode query 
+	selectStatID  = ("SELECT ID FROM EOT_DATA.MStatus WHERE MStatus.statCode = %d" % statcode) ##Select status ID via statCode query 
 	##Insert measurement data query
 	add_measurement = ("INSERT INTO EOT_DATA.Measurement (m_time, statusID, m_data, unitID, photoID) VALUES (%(m_time)s, %(statusID)s, %(m_data)s, %(unitID)s , %(photoID)s)")
 
+	SelectUnitID = ("SELECT ID FROM EOT_DATA.Unit WHERE Unit.u_type LIKE '%s'" % unitType) ##Select unit ID via unit type
+	
+	SelectCameraID = ("SELECT ID FROM EOT_DATA.Camera WHERE Camera.node = %(node)s AND Camera.locationID = %(locationID)s") ##Select camera ID via camera node
 
+	SelectLocationID = ("SELECT ID FROM EOT_DATA.Location WHERE Location.address LIKE '%s'" % locationAddress) ##Select location ID via location address
 
+	add_camera = ("INSERT INTO EOT_DATA.Camera (node, locationID) VALUES (%(node)s , %(locationID)s)") ##Add new camera
+	add_location  = ("INSERT INTO EOT_DATA.Location (address) VALUES ('%s')" % locationAddress) ##Add new location
+	add_unit  = ("INSERT INTO EOT_DATA.Unit (u_type) VALUES ('%s')" % unitType) ##Add new unit
 	##End Queries ######
-
 
 	try:
 
 		con = connect() ##create CONNECTION
 		cursor = con.cursor() ##setup cursor
 
+		cursor.execute(SelectLocationID)
+		locationID_data = cursor.fetchone()
+		if locationID_data is None:
+			cursor.execute(add_location)
+			cursor.execute(SelectLocationID) ##Select camera ID
+			locationID = cursor.fetchone()[0] ##Assign photo ID into variable 'cameraID'
+		elif locationID_data is not None:
+			locationID = locationID_data[0] ##Assign photo ID into variable 'cameraID'
+		
+		##end if else
 
-		cursor.execute(add_photo) ##add photo into database with its path
+
+		data_camera={
+			'node': cameraNode,
+			'locationID' : locationID,
+		}
+
+
+		cursor.execute(SelectCameraID, data_camera) ##Select camera ID
+		cameraID_data = cursor.fetchone()
+		if cameraID_data is None:
+			cursor.execute(add_camera, data_camera)
+			cursor.execute(SelectCameraID, data_camera) ##Select camera ID
+			cameraID = cursor.fetchone()[0] ##Assign photo ID into variable 'cameraID'
+		elif cameraID_data is not None:
+			cameraID = cameraID_data[0] ##Assign photo ID into variable 'cameraID'
+		
+		##end if else
+
+		
+		data_photo={
+			'path'      : photoPath,
+			'cameraid'  : cameraID,
+		}
+
+
+		cursor.execute(add_photo, data_photo) ##add photo into database with its path
 		cursor.execute(selectPhotoID) ##Select related photo's id
 		photoid = cursor.fetchone()[0] ##Assign related photo's id into variable 'photoid'
 
 		cursor.execute(selectStatID) ##Select related status ID
-		statID  =  cursor.fetchone()[0] ##Assign related status ID into variable 'statID'
+		statID  = cursor.fetchone()[0] ##Assign related status ID into variable 'statID'
+
+		cursor.execute(SelectUnitID) ##Select related unit ID
+		unitid_data = cursor.fetchone() ##Assign related unit ID into variable unitID
+		if unitid_data is None:
+			cursor.execute(add_unit)
+			cursor.execute(SelectUnitID) ##Select unit ID
+			unitid = cursor.fetchone()[0] ##Assign unit ID into variable 'unitid'
+		elif unitid_data is not None:
+			unitid = unitid_data[0] ##Assign photo ID into variable 'cameraID'
+
+
 
 		data_measurement= {  	##data of measurement will insert into database
 			'm_time'  : mtime,
@@ -125,9 +176,10 @@ def insert_data(mtime, mdata, photoPath):
 	
 		con.rollback() ##rollback all the operation
 		cursor.close() ##close the cursor
-		con.close() ##At last, close the database connection
+		con.close() ##close the database connection
 		print('Data has not inserted. Rollback!')
 	##end try / except
+
 
 ###END OF THE FUNCTION
 #######################################################################
